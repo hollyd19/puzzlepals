c2760cfc23b40256532088d3d2f169fb13a4037b
Nicole Cooprider <nmc58@cornell.edu>
2013-05-11 15:45:24 -0400

    hmmm...

--- javascript/javascript-dragndrop.js
+++ javascript/javascript-dragndrop.js
@@ -2,12 +2,14 @@ $(document).ready(function () {
     var $box = $("#box"),
         $puzzle = $("#puzzle");
         
+    if ($("element").length == 0){
     // let the box items be draggable
     $(".piece", $box).draggable({
         cursor: "move",
         snap: ".place",
         snapMode: "corner"
     });
+    }
     
     
     $(".place", $puzzle).droppable({
