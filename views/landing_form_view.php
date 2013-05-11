bc264ed68ca87368e2adc8b2f32cd3e1760dfc99
Nicole Cooprider <nmc58@cornell.edu>
2013-05-11 00:13:03 -0400

    hmmm...

--- views/landing_form_view.php
+++ views/landing_form_view.php
@@ -115,6 +115,7 @@
 			for($a=0; $a<sizeof($medium); $a++){
 				$var= $medium[$a];
 				$string_of_players="";
+				var_dump($var['users']);
 				foreach($var['users'] as $player1){
 					$string_of_players= $string_of_players + $player1 + "<br/>";
 				}
