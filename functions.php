b5d715d85404a3c78b99525e14f2907d8f9062c2
Nicole Cooprider <nmc58@cornell.edu>
2013-05-12 21:47:11 -0400

    asynchronous

--- functions.php
+++ functions.php
@@ -256,7 +256,7 @@
 		$new_time= time(); - 5;
 		//echo $puzzle_id;
 		//echo " ". $new_time;
-		$cursor= $collection->find(array("puzzleID"=>$puzzle_id));
+		$cursor= $collection->find(array("puzzleID"=>$puzzle_id, "lastUPDATED"=>array("\$lt"=>$new_time)));
 		$result= array();
 		foreach($cursor as $document){
 			$doc_info= array();
@@ -264,7 +264,7 @@
 			$doc_info['y']=$document['y'];
 			$doc_info['imgURL']=$document['imgURL'];
 			$doc_info['lastUPDATED']=$document['lastUPDATED'];
-			$doc_info['time now']=time();
+
 			array_push($result, $doc_info);
 		}
 		//$return = $time . " " . $new_time;
