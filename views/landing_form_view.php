f67b62b3dc31c74a46a9e9488c5d98bb5c9d1bbb
Holly Domke <hed35@cornell.edu>
2013-05-13 02:35:26 -0400

    rearranging completed puzzles print outs

--- views/landing_form_view.php
+++ views/landing_form_view.php
@@ -182,11 +182,16 @@
 
 	<div class="row-fluid">
 	<div class="span12">
-		<ul>
+		
+		<?php 
+			echo '<h4>Total Completed:' . sizeof($completed_puzzle_list) . '</h4>'; 
+		?>
+		
+		
 		<?php
-			echo sizeof($completed_puzzle_list); 
+			
 			foreach($completed_puzzle_list as $var){
-				$string_of_players="hwllo ";
+				$string_of_players="";
 				$item= $var['users'];
 				foreach($item as $player1){
 					$string_of_players= $string_of_players . $player1 . " and";
@@ -195,14 +200,18 @@
 				$your_date = $var["time"];
 				$datediff = $now - $your_date;
 				$datediff= time_elapsed($datediff);
-				echo '<li><ul><li><img src ="'.$var["name"].'.png " class="" alt="' . $var["name"] . '" /></li>';
-				echo '<li>'.$datediff.'</li>';
-				echo  '<li>'.$string_of_users.'</li></ul></li>';
-				
-				
+				echo '<div class="row-fluid">';
+				echo '<img class="span6" src ="'.$var["name"].'.png " class="" alt="' . $var["name"] . '" />';
+				echo '<div class=span5">';
+				echo '<h6>Time Taken:</h6><p?' . $datediff . '</p>';
+				if(!is_null($string_of_users)) {
+					echo  '<h6>Participants</h6><p>'.$string_of_users.'</p>';
+				}
+				echo '</div>';
+				echo '</div>';
 			}
 		?>
-		</ul>
+		
 	</div>
 	</div>
 </div>
