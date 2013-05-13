76b3bbc4b90c66634e256dfebf4014a18945b86d
Holly Domke <hed35@cornell.edu>
2013-05-13 01:25:28 -0400

    merp

--- views/landing_form_view.php
+++ views/landing_form_view.php
@@ -65,7 +65,7 @@
 						}
 					
 					echo '</div>';
-					echo '<div class="span5" id="select_difficulty">';
+					echo '<div class="span6" id="select_difficulty">';
 					echo '<h4 class="instructions">2. Select Difficulty</h4>';
 					echo '<input type="radio" name="puzzle_size" value="9" checked>Easy<br/>';
 					echo '<input type="radio" name="puzzle_size" value="25">Medium<br/>';
@@ -87,15 +87,15 @@
 					//echo '</ul>';
 					echo '</div>';
 					
-					echo '<div id="who_you_invited" class="span3"></div>';
-					
-					
-					
 					//echo '<div class="span5" id="invite_friends_div"><br/>';
 					//echo '<a href="#" class="apprequests" id="sendRequest" data-message="Come play Puzzle Pals!">';
 					//echo '<span class="btn btn-primary apprequests">3. Invite Friends</span></a></div>'; 
 				
-				echo '<div class="span5" id="create_button_div"><br/><input class="btn btn-primary" type="submit" value="4. Create & Go!" name="create"></div></div>';
+				echo '<div class="span5" id="create_button_div"><br/><input class="btn btn-primary" type="submit" value="4. Create & Go!" name="create"></div>';
+				
+				echo '<div id="who_you_invited" class="span3"></div>';
+				
+				echo'</div>';
 				?>
 		
 		</form>
