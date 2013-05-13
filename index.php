dddf825d24f0327aac52bd64498de2d8df497c5a
Holly Domke <hed35@cornell.edu>
2013-05-13 01:00:45 -0400

    blah

--- index.php
+++ index.php
@@ -156,7 +156,10 @@ $app_name = idx($app_info, 'name', '');
 					console.log(data);
 					$.each(data["data"], function(user, info) {
 						console.log(info);
+						console.log(info["name"]);
+						console.log(info.name);
                         names.push(info["name"]);
+						
 						console.log(names);
                     });
                     if (names.length) {
