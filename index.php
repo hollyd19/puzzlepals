64bfb568dcc5022d50d0c70f00aaf13686997062
Holly Domke <hed35@cornell.edu>
2013-05-13 00:47:43 -0400

    nicole is awesome part 2

--- index.php
+++ index.php
@@ -155,7 +155,8 @@ $app_name = idx($app_info, 'name', '');
 					var names = [];
 					console.log(data);
 					$.each(data["data"], function(user) {
-                        names.push(user["name"]);
+						console.log(user.name);
+                        names.push(user.name);
 						console.log(names);
                     });
                     if (names.length) {
